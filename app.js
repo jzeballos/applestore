ddf8c7ec60c777042e4bbeb2328c9b00088c0842
jzeballos <jizeball@gmail.com>
2017-02-11 11:34:22 -0700

    done

--- app.js
+++ app.js
@@ -6,66 +6,99 @@
 
 
 	var apple = angular.module('apple', []); // creates a module
-	// angular.module('apple'); // gets a reference to an existing module
 
-	apple.controller('MacController', function (
-		$scope,
-		productsService
-	) {
+	apple.controller('MacController', function ($scope, productsService) {
 		$scope.title = 'The Awesome Apple App';
-
-		$scope.featureProduct = {
-			name: 'Macbook Pro',
-			price: 3000
-		};
-
-		$scope.products = productsService.getProducts();
 
 		$scope.doSubmit = function () {
 			productsService.addProduct(
 				$scope.myInputName, $scope.myInputPrice
 			);
 		}
+
+		$scope.getProducts = function(){
+        $scope.products = productsService.getProducts();
+    }
+
+    $scope.getProducts();
+		$scope.featureProduct = productsService.getFeatured();
+		$scope.selectFeatured = function(index) {
+			productsService.selectFeatured(index);
+			$scope.featureProduct = productsService.getFeatured();
+		}
+
 	});
+
 
 	apple.service('productsService', function () {
 
 		var products = [
 			{
 				name: 'Macbook Pro 15 inch',
-				price: 3000
+				price: 3000,
+				featured: false
 			},
 			{
 				name: 'Macbook Pro 13 inch',
-				price: 1699.99
+				price: 1699.99,
+				featured: false
 			},
 			{
 				name: 'iPad Pro',
-				price: 999.99
+				price: 999.99,
+				featured: false
 			},
 			{
 				name: 'iPad',
-				price: 799.99
+				price: 799.99,
+				featured: false
 			},
 			{
 				name: 'iPhone',
-				price: 679.99
+				price: 679.99,
+				featured: false
 			}
 		];
+
 
 		this.getProducts = function () {
 			// check authentication
 			return products;
 		}
 
+		this.selectFeatured = function(index) {
+			// products[index].featured = !products[index].featured;
+			for (var i in products) {
+					products[i].featured = false;
+			}
+			products[index].featured = true;
+
+		}
+
 		this.addProduct = function (name, price) {
-			console.log('this defective service function fired!');
+			// console.log('this defective service function fired!');
 			products.push({
 				name: name,
 				price: price
 			});
 		}
+
+		this.getFeatured = function() {
+			for (var i in products) {
+				if (products[i].featured === true) {
+					return products[i];
+				}
+			}
+		}
 	});
+
+
+
+
+
+
+
+
 
 	var ms = angular.module('microsoft', []);
 })();
@@ -75,8 +108,8 @@
 // we register a module with angular
 
 
-/* 
-	
+/*
+
 */
 // the angular object has functions lime module
-// 
+//
